290ef4d3d59390dee21fb4c578bd5c384e397300
son.sssonya <Antonova.S1@edu.rea.ru>
2026-05-01 04:52:49 +0300

    Test 2 for backend starting

---
+++ backend/src/balances/balances.controller.spec.ts
@@ -0,0 +1,36 @@
+﻿import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from '../app.module';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('BalancesController (e2e)', () => {
+  let app: INestApplication;
+  let prismaService: PrismaService;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    prismaService = moduleFixture.get<PrismaService>(PrismaService);
+    await app.init();
+  });
+
+  beforeEach(async () => {
+    await prismaService.cleanDatabase();
+  });
+
+  afterAll(async () => {
+    await prismaService.cleanDatabase();
+    await app.close();
+  });
+
+  it('should work', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/api/groups/123/balances');
+    
+    expect(response.status).toBe(404);
+  });
+});
